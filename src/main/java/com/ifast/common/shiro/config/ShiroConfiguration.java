3ca451e6876d8b07e0d57d1f6d41b8765b31a57b
dahao <dahao_liang@jinbag.com>
2019-04-19 17:51:46 +0800

    shiro添加记住我功能

--- src/main/java/com/ifast/common/shiro/config/ShiroConfiguration.java
+++ src/main/java/com/ifast/common/shiro/config/ShiroConfiguration.java
@@ -14,6 +14,7 @@ import org.apache.shiro.authc.pam.AtLeastOneSuccessfulStrategy;
 import org.apache.shiro.authz.Authorizer;
 import org.apache.shiro.authz.ModularRealmAuthorizer;
 import org.apache.shiro.cache.CacheManager;
+import org.apache.shiro.codec.Base64;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.session.SessionListener;
@@ -22,6 +23,7 @@ import org.apache.shiro.session.mgt.eis.SessionDAO;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.servlet.SimpleCookie;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
@@ -37,38 +39,75 @@ import javax.servlet.Filter;
 import java.util.*;
 
 /**
- * <pre>
  * . cache ehcache
  * . realm(cache)
  * . securityManager（realm）
  * . ShiroFilterFactoryBean 注册
  *
- * </pre>
- * <small> 2018年4月18日 | Aron</small>
+ * @author dahao
  */
 @Configuration
 public class ShiroConfiguration {
 
     @Bean
-    SessionDAO sessionDAO(ShiroProperties config) {
-//        EnterpriseCacheSessionDAO sessionDAO = new EnterpriseCacheSessionDAO();
-//        CacheManager cacheManager = enterpriseCacheSessionDAO.getCacheManager();
-//        return enterpriseCacheSessionDAO;
+    ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        shiroFilterFactoryBean.setSuccessUrl("/admin/index");
+        shiroFilterFactoryBean.setUnauthorizedUrl("/shiro/405");
 
-        RedisSessionDAO sessionDAO = new RedisSessionDAO(config.getSessionKeyPrefix());
+        LinkedHashMap<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
 
-//        SessionDAO sessionDAO = new MemorySessionDAO();
-        return sessionDAO;
+        filterChainDefinitionMap.put("/api/**", "jwt"); // api
+        filterChainDefinitionMap.put("/emil/**", "anon");
+        filterChainDefinitionMap.put("/doc.html**", "anon");
+        filterChainDefinitionMap.put("/swagger-resources/**", "anon");
+        filterChainDefinitionMap.put("/webjars/**", "anon");
+        filterChainDefinitionMap.put("/v2/**", "anon");
+        filterChainDefinitionMap.put("/page/**", "anon");
+        filterChainDefinitionMap.put("/article/**", "anon");
+        filterChainDefinitionMap.put("/event/**", "anon");
+        filterChainDefinitionMap.put("/shiro/**", "anon");
+        filterChainDefinitionMap.put("/login", "anon");
+        filterChainDefinitionMap.put("/css/**", "anon");
+        filterChainDefinitionMap.put("/js/**", "anon");
+        filterChainDefinitionMap.put("/fonts/**", "anon");
+        filterChainDefinitionMap.put("/img/**", "anon");
+        filterChainDefinitionMap.put("/video/**", "anon");
+        filterChainDefinitionMap.put("/docs/**", "anon");
+        filterChainDefinitionMap.put("/druid/**", "anon");
+        filterChainDefinitionMap.put("/upload/**", "anon");
+        filterChainDefinitionMap.put("/files/**", "anon");
+        filterChainDefinitionMap.put("/test/**", "anon");
+        filterChainDefinitionMap.put("/logout", "logout");
+        filterChainDefinitionMap.put("/", "anon");
+        filterChainDefinitionMap.put("/**", "user");
+
+        // 添加jwt过滤器
+        Map<String, Filter> filterMap = new HashMap<>();
+        filterMap.put("jwt", new JWTAuthenticationFilter());
+        shiroFilterFactoryBean.setFilters(filterMap);
+
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return shiroFilterFactoryBean;
     }
 
     @Bean
-    public SimpleCookie sessionIdCookie(ShiroProperties shiroConfigProperties) {
-        return new SimpleCookie(shiroConfigProperties.getJsessionidKey());
+    SessionDAO sessionDAO(ShiroProperties config) {
+        return new RedisSessionDAO(config.getSessionKeyPrefix());
+    }
+
+    @Bean
+    public SimpleCookie rememberMeCookie() {
+        SimpleCookie cookie = new SimpleCookie("rememberMe");
+        cookie.setMaxAge(86400);
+        return cookie;
     }
 
     @Bean
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<Object, Object> template = new RedisTemplate<Object, Object>();
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
 //        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer( Object.class );
@@ -83,13 +122,13 @@ public class ShiroConfiguration {
     @Bean
     public SessionManager sessionManager(SessionDAO sessionDAO, SimpleCookie simpleCookie) {
         DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
-        sessionManager.setSessionIdCookie(simpleCookie);
-
         Collection<SessionListener> sessionListeners = new ArrayList<>();
         sessionListeners.add(new BDSessionListener());
         sessionManager.setSessionListeners(sessionListeners);
         sessionManager.setSessionDAO(sessionDAO);
         sessionManager.setGlobalSessionTimeout(24 * 60 * 60 * 1000);
+        // 去掉 JSESSIONID
+        sessionManager.setSessionIdUrlRewritingEnabled(false);
         return sessionManager;
     }
 
@@ -130,50 +169,21 @@ public class ShiroConfiguration {
         manager.setAuthorizer(authorizer);
         manager.setCacheManager(cacheManager());
         manager.setSessionManager(sessionManager);
+        manager.setRememberMeManager(rememberMeManager());
         return manager;
     }
 
-    @Bean
-    ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
-        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
-
-        shiroFilterFactoryBean.setSecurityManager(securityManager);
-        shiroFilterFactoryBean.setLoginUrl("/login");
-        shiroFilterFactoryBean.setSuccessUrl("/admin/index");
-        shiroFilterFactoryBean.setUnauthorizedUrl("/shiro/405");
-        LinkedHashMap<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
-        filterChainDefinitionMap.put("/api/**", "jwt"); // api
-        filterChainDefinitionMap.put("/emil/**", "anon");
-        filterChainDefinitionMap.put("/doc.html**", "anon");
-        filterChainDefinitionMap.put("/swagger-resources/**", "anon");
-        filterChainDefinitionMap.put("/webjars/**", "anon");
-        filterChainDefinitionMap.put("/v2/**", "anon");
-        filterChainDefinitionMap.put("/page/**", "anon");
-        filterChainDefinitionMap.put("/article/**", "anon");
-        filterChainDefinitionMap.put("/event/**", "anon");
-        filterChainDefinitionMap.put("/shiro/**", "anon");
-        filterChainDefinitionMap.put("/login", "anon");
-        filterChainDefinitionMap.put("/css/**", "anon");
-        filterChainDefinitionMap.put("/js/**", "anon");
-        filterChainDefinitionMap.put("/fonts/**", "anon");
-        filterChainDefinitionMap.put("/img/**", "anon");
-        filterChainDefinitionMap.put("/video/**", "anon");
-        filterChainDefinitionMap.put("/docs/**", "anon");
-        filterChainDefinitionMap.put("/druid/**", "anon");
-        filterChainDefinitionMap.put("/upload/**", "anon");
-        filterChainDefinitionMap.put("/files/**", "anon");
-        filterChainDefinitionMap.put("/test/**", "anon");
-        filterChainDefinitionMap.put("/logout", "logout");
-        filterChainDefinitionMap.put("/", "anon");
-        filterChainDefinitionMap.put("/**", "authc");
-
-        // 添加jwt过滤器
-        Map<String, Filter> filterMap = new HashMap<>();
-        filterMap.put("jwt", new JWTAuthenticationFilter());
-        shiroFilterFactoryBean.setFilters(filterMap);
-
-        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
-        return shiroFilterFactoryBean;
+    /**
+     * cookie管理对象
+     *
+     * @return
+     */
+    public CookieRememberMeManager rememberMeManager() {
+        CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
+        cookieRememberMeManager.setCookie(rememberMeCookie());
+        // rememberMe cookie加密的密钥
+        cookieRememberMeManager.setCipherKey(Base64.decode("3AvVhmFLUs0KTA3Kprsdag=="));
+        return cookieRememberMeManager;
     }
 
     @Bean
